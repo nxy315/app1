ad8ae8ab0a1630cd379646e0dfc1bf6b2383fb23
nxy <xiaoyang91315@sina.com>
2017-12-15 19:20:11 +0800

    up

--- pages/center/center.js
+++ pages/center/center.js
@@ -9,7 +9,7 @@ Page({
   data: {
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    // canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
 
   openSettion: function() {
@@ -34,17 +34,6 @@ Page({
     wx.setNavigationBarTitle({
       title: '个人中心'
     })
-
-    
-  },
-
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
   },
 
   /**
@@ -58,32 +47,55 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      // wx.getUserInfo({
-        // success: res => {
-          app.globalData.userInfo = null
+    console.log(app.globalData.userInfo)
+
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              app.globalData.userInfo = res.userInfo
+              this.setData({
+                userInfo: app.globalData.userInfo,
+                hasUserInfo: true
+              })
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              // if (this.userInfoReadyCallback) {
+              //   this.userInfoReadyCallback(res)
+              // }
+            }
+          })
+        } else {
+          app.globalData.userInfo = null;
           this.setData({
             userInfo: null,
             hasUserInfo: false
           })
-        // }
-      // })
-    }
+        }
+      }
+    })
+    // if (app.globalData.userInfo) {
+    //   console.log(1);
+    //   this.setData({
+    //     userInfo: app.globalData.userInfo,
+    //     hasUserInfo: true
+    //   })
+    // }  else {
+    //   console.log(3);
+    //   // 在没有 open-type=getUserInfo 版本的兼容处理
+    //   // wx.getUserInfo({
+    //     // success: res => {
+    //       app.globalData.userInfo = null
+    //       this.setData({
+    //         userInfo: null,
+    //         hasUserInfo: false
+    //       })
+    //     // }
+    //   // })
+    // }
   },
 
   /**
--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -7,7 +7,24 @@ Page({
   data: {
   
   },
+  rander: function () {
+    if (tmpAngle >= endAngle) {
+      return;
+    } else if (tmpAngle + xAngle > endAngle) {
+      tmpAngle = endAngle;
+    } else {
+      tmpAngle += xAngle;
+    }
+    ctx.clearRect(0, 0, mW, mH);
 
+    //画圈
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = '#1c86d1';
+    ctx.arc(r, r, cR, startAngle, tmpAngle);
+    ctx.stroke();
+    ctx.closePath();
+  },
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,14 +36,30 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+    var c = document.getElementById('myCanvas');
+    var ctx = c.getContext('2d');
+
+    var mW = c.width = 300;
+    var mH = c.height = 300;
+    var lineWidth = 5;
+    var r = mW / 2; //中间位置
+    var cR = r - 4 * lineWidth; //圆半径
+    var startAngle = -(1 / 2 * Math.PI); //开始角度
+    var endAngle = startAngle + 2 * Math.PI; //结束角度
+    var xAngle = 1 * (Math.PI / 180); //偏移角度量
+    var fontSize = 35; //字号大小
+    var tmpAngle = startAngle; //临时角度变量
+
+    //渲染函数
+    
+    this.rander();
   },
 
   /**
